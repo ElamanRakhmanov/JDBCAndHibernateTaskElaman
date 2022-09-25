2a4b91774675b2fd9776726685d36878aa8c739d
Lenovo <ElamanRakhmanov@gmail.com>
2022-09-25 23:44:35 +0300

    commit

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDao.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDao.java
@@ -6,7 +6,20 @@ import java.util.List;
 
 public interface UserDao {
 
-    void createUsersTable();
+    public static void createUsersTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS Users(" +
+                "id LONG PRIMARY KEY," +
+                "name VARCHAR(50) NOT NULL," +
+                "lastName VARCHAR(50) NOT NULL," +
+                "age BYTE)"
+        try (Connection connect = Util.connect();;
+        Statement statement = connect.createStatement()){
+            statement.executeUpdate(sql);
+            System.out.println("Table successfully created" + Users);
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
 
     void dropUsersTable();
 
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
@@ -1,5 +1,18 @@
 package peaksoft.util;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String url = "jdbc:postgresql://localhost:5432/jdbc-practice";
+    public static final String username = "postgres";
+    public static final String password = "2000";
+
+    public static Connection connect(){
+        Connection connect = null;
+        try{
+            connect = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to db successfully");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return connect;
+    }
 }
